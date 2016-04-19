5e28f044e97577cad78f39f612bbc922a03cea55
unicorn515 <lengch@atmb.cn>
2016-04-19 15:23:12 +0800

    Update test1.js

--- doc/test1.js
+++ doc/test1.js
@@ -1,11 +1,9 @@
-// 数据缓存
-// 建议开发者选择mvvm框架来通过数据来驱动UI变化
-(function(){
+
 			 var bt=document.getElementById("signinb");
 			 var pt=document.getElementById("signinno");
 			 $('#signinb').onclick=function(){
 			  localStorage.setItem("vinput",$('#signinno').val());
 			  location.href='http://vloz-zshc.daoapp.io/end';
 			 }
-})();
+
 

c119e413fd76df3697dd196f9e902e1811cf90dc
hh <hh@hh>
2016-04-19 23:42:59 +0800

    xxxxz

--- target/init/mdb.js
+++ target/init/mdb.js
@@ -7,7 +7,8 @@ var _mongoose2 = _interopRequireDefault(_mongoose);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = function (done) {
-  const conn = _mongoose2.default.createConnection($.config.get('db.mongodb'));
+  //const conn=mong.createConnection($.config.get('db.mongodb'));
+  const conn = _mongoose2.default.createConnection(process.env.MONGODB_CONNECTION);
   $.mongodb = conn;
   $.model = {};
   const ObjectId = _mongoose2.default.Types.ObjectId;

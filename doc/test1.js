1cb606714c308dbeb501a8a645a89b2e9e34def6
hh <hh@hh>
2016-04-19 18:57:44 +0800

    deldoc

--- doc/test1.js
+++
@@ -1,9 +0,0 @@
-(function(){
-			 //var bt=document.getElementById("signinb");
-			 //var pt=document.getElementById("signinno");
-			 console.log('im');
-
-			  localStorage.setItem("vinput",'ali');
-			  location.href='http://vloz-zshc.daoapp.io/end';
-})();
-

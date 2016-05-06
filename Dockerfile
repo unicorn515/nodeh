4b233c5b4db62c9bdaa4086c909d86be9bd0ba1e
leng <lengch@atmb.cn>
2016-05-06 23:53:40 +0800

    x

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node
-RUN mkdir -p /usr/src
-WORKDIR /usr/src
-COPY . /usr/src
-RUN npm install
-EXPOSE 3000
-ENTRYPOINT node target/server.js
--- target/config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function (set, get, has) {
-  set('web.port', 3000);
-  //set('db.mongodb','mongodb://127.0.0.1/db1');
-  set('web.session.secret', 'test');
-};
